8b1077031058d519fa6378f4c53ed2ec835c1e35
theli-ua <theli.ua@gmail.com>
2011-12-07 21:38:25 +0200

    inhouse plugin

---
+++ modules/inhouse.py
@@ -0,0 +1,57 @@
+"""
+should announce inhouses
+and track games people play together
+"""
+
+from hon.packets import ID
+
+class Game:
+    def __init__(self,gamename,matchid,server):
+        self.name = gamename
+        self.matchid = matchid
+        self.server = server
+        self.announced = False
+        self.time = None
+        self.players = set()
+_games = {}
+_id2game = {}
+_min_players = 2
+
+def _add_game(account_id,game_name,matchid,server):
+    key = (matchid,game_name)
+    if key not in _games:
+        _games[key] = Game(game_name,matchid,server)
+    _games[key].players |= set([account_id])
+    _id2game[account_id] = key
+
+def _del_game(account_id):
+    if account_id in _id2game:
+        key = _id2game[account_id]
+        _games[key].players -= set([account_id])
+        if len(_games[key].players) == 0:
+            del _games[key]
+        del _id2game[account_id]
+
+def status_update(bot,packet_id,data):
+    #id,status,flags,clan_id,clan_name,chatsymbol,shield,icon
+    #if ingame -> server,game name, matchid
+    if data[1] == ID.HON_STATUS_INGAME:
+        _add_game(data[0],data[9],data[10],data[8])
+    else:
+        _del_game(data[0])
+status_update.event = [ID.HON_SC_UPDATE_STATUS]
+
+def initiall_statuses(bot,packet_id,data):
+    #id,status,flags
+    #server, gamename
+    for u in data[1]:
+        if u[1] in [ ID.HON_STATUS_INLOBBY , ID.HON_STATUS_INGAME ]:
+            _add_game(u[0],u[4],0,u[3])
+initiall_statuses.event = [ID.HON_SC_INITIAL_STATUS]
+
+def ih(bot,input):
+    for game in _games.values():
+        if len(game.players) >= _min_players:
+            players = [bot.id2nick[id] for id in game.players]
+            bot.say('{0} [{1}]'.format(game.name,','.join(players)))
+ih.commands = ['ih']
