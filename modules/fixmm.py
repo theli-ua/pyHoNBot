5518262565433448f0553d10c7e2c4219d377cc4
theli-ua <theli.ua@gmail.com>
2012-01-31 08:03:17 +0200

    mm invite working finally

--- modules/fixmm.py
+++ modules/fixmm.py
@@ -2,9 +2,9 @@ from hon.packets import ID
 
 VERSION = '2.5.8'
 
-def initiall_statuses(bot,packet_id,data):
+def startgroup(bot,packet_id,data):
     bot.write_packet(ID.HON_CS_START_MM_GROUP,VERSION,0x0102,'caldavar','sd|bd|bp|','EU|',0x0001)
-initiall_statuses.event = [ID.HON_SC_INITIAL_STATUS]
+startgroup.event = [ID.HON_SC_INITIAL_STATUS]
 
 
 def fixmm(bot,input):
