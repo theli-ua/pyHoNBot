e49f2613740fb8ac6fd47c5e786a71c00454bcfa
Anton Romanov <theli.ua@gmail.com>
2012-03-14 11:10:00 +0200

    mm version from patchserver

--- modules/fixmm.py
+++ modules/fixmm.py
@@ -1,8 +1,8 @@
 from hon.packets import ID
 
-VERSION = '2.5.14'
 
 def startgroup(bot,packet_id,data):
+    VERSION = '.'.join(bot.stringtable_version.split('.')[:3])
     bot.write_packet(ID.HON_CS_START_MM_GROUP,VERSION,0x0102,'caldavar','sd|bd|bp|','EU|',0x0001)
 startgroup.event = [ID.HON_SC_INITIAL_STATUS]
 
