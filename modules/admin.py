c3da75bab265fd6b10b20d0916aa7baa3dd9c859
Anton Romanov <theli.ua@gmail.com>
2012-03-20 15:03:55 +0200

    ignore list management

--- modules/admin.py
+++ modules/admin.py
@@ -14,3 +14,15 @@ def part(bot, input):
     bot.write_packet(ID.HON_CS_LEAVE_CHANNEL,input.group(2))
     bot.config.set_del('channels',input.group(2))
 part.commands = ['part']
+
+def ignore(bot, input): 
+    """makes bot ignore user, admins only""" 
+    if not input.admin: return
+    bot.config.set_add('ignore',input.group(2))
+ignore.commands = ['ignore']
+
+def unignore(bot, input): 
+    """makes bot stop ignoring user, admins only""" 
+    if not input.admin: return
+    bot.config.set_del('ignore',input.group(2))
+unignore.commands = ['unignore']
