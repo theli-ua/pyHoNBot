834454e2dac701f6fdd45a93e326544ddc04bf1d
Anton Romanov <theli.ua@gmail.com>
2012-03-21 17:32:29 +0200

    make welcome to clan optional

--- modules/clan.py
+++ modules/clan.py
@@ -1,10 +1,13 @@
 # -*- coding: utf8 -*-
 from hon.packets import ID
 
+def setup(bot):
+    bot.config.module_config('welcome_members',[1,'Will welcome members in /c m if set to non-zero value'])
+
 def add_member(bot,origin,data):
     id = data[0]
     bot.clan_roster[id] = {"rank":"Member"}
-    if id in bot.id2nick:
+    if bot.config.welcome_members > 0 and id in bot.id2nick:
         nick = bot.id2nick[id]
         bot.write_packet(ID.HON_CS_CLAN_MESSAGE,'Welcome, {0}!'.format(nick))
 add_member.event = [ID.HON_SC_CLAN_MEMBER_ADDED]
