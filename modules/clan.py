2f8dcb9afb5ee9b4faef67f2bd5b0eac0c65142f
Anton Romanov <theli.ua@gmail.com>
2012-03-23 17:52:31 +0200

    track clan member status changes

--- modules/clan.py
+++ modules/clan.py
@@ -4,15 +4,19 @@ from hon.packets import ID
 def setup(bot):
     bot.config.module_config('welcome_members',[1,'Will welcome members in /c m if set to non-zero value'])
 
-def add_member(bot,origin,data):
-    id = data[0]
-    bot.clan_roster[id] = {"rank":"Member"}
-    if bot.config.welcome_members > 0 and id in bot.id2nick:
-        nick = bot.id2nick[id]
-        bot.write_packet(ID.HON_CS_CLAN_MESSAGE,'Welcome, {0}!'.format(nick))
-add_member.event = [ID.HON_SC_CLAN_MEMBER_ADDED]
-        
-def del_member(bot,origin,data):
-    del(bot.clan_roster[data[0]])
-del_member.event = [ID.HON_SC_CLAN_MEMBER_LEFT]
+def change_member(bot,origin,data):
+    who,status,whodid = data[0],data[1],data[2]
+    if status == 0:
+        del(bot.clan_roster[who])
+    elif status == 1:
+        if who in bot.clan_roster:
+            bot.clan_roster[who]['rank'] = 'Member'
+        else:
+            bot.clan_roster[who] = {"rank":"Member"}
+    elif status == 2:
+        bot.clan_roster[who]['rank'] = 'Officer'
+    elif status == 3:#not sure about this one
+        bot.clan_roster[who]['rank'] = 'Leader'
+
+change_member.event = [ID.HON_SC_CLAN_MEMBER_CHANGE]
 
