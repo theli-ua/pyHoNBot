dc7e72b4cc3fc82cc9670773826c04202d07a098
Anton Romanov <theli.ua@gmail.com>
2012-08-17 17:02:12 +0300

    mhm

--- modules/fixmm.py
+++ modules/fixmm.py
@@ -6,6 +6,12 @@ def startgroup(bot,origin,data):
     bot.write_packet(ID.HON_CS_START_MM_GROUP,VERSION,0x0102,'caldavar','sd|','EU|',0x0001)
 startgroup.event = [ID.HON_SC_INITIAL_STATUS]
 
+def startgroup2(bot,input):
+    if not input.owner:return
+    VERSION = '.'.join(bot.stringtable_version.split('.')[:3])
+    bot.write_packet(ID.HON_CS_START_MM_GROUP,VERSION,0x0102,'caldavar','sd|','EU|',0x0001)
+startgroup2.commands = ['refixmm']
+
 def fixmm(bot,input):
     bot.write_packet(ID.HON_CS_INVITE_TO_MM,input.nick)
 fixmm.commands = ['fixmm']
