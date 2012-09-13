2408009d229589824800ed9a9e4092e97bd23a4b
Ninja101 <pbninja101@gmail.com>
2012-09-13 02:42:09 +0100

    more fixes

--- modules/forumcheck.py
+++ modules/forumcheck.py
@@ -16,17 +16,27 @@ def applycheck(bot, input):
 			thread = threadinfo['thread']
 			if thread['preview'].lower().find(input.nick.lower()) > 0 and thread['prefix_rich'].find("APPROVED"):
 				bot.reply("Welcome to Project Epoch, %s! Inviting now." % input.nick)
-				bot.write_packet(ID.HON_CS_CLAN_ADD_MEMBER,input.account_id)
-				bot.vb.NewPost( thread['threadid'], "Invited", "Player has been invited to the clan.")
+				if not input.account_id in self.clan_roster:
+					bot.write_packet(ID.HON_CS_CLAN_ADD_MEMBER,input.account_id)
+					bot.reply("Invited!")
+					bot.vb.NewPost( thread['threadid'], "Invited", "Player has been invited to the clan.")
+				else:
+					bot.reply("You're already in the clan?!")
+					bot.vb.NewPost( thread['threadid'], "Invited", "Player has been invited to the clan. (By someone else)")
 				bot.vb.MoveThread( thread['threadid'], 36 )
 				return
 		mentorApps = bot.vb.GetThreads(35, 30)
 		for threadinfo in mentorApps:
 			thread = threadinfo['thread']
 			if thread['preview'].lower().find(input.nick.lower()) > 0 and thread['prefix_rich'].find("APPROVED"):
-				bot.reply("Welcome to Project Epoch, %s! Inviting now." % input.nick)
-				bot.write_packet(ID.HON_CS_CLAN_ADD_MEMBER,input.account_id)
-				bot.vb.NewPost( thread['threadid'], "Invited", "Player has been invited to the clan.")
+				bot.reply("Welcome to Project Epoch, %s!" % input.nick)
+				if not input.account_id in self.clan_roster:
+					bot.write_packet(ID.HON_CS_CLAN_ADD_MEMBER,input.account_id)
+					bot.reply("Invited!")
+					bot.vb.NewPost( thread['threadid'], "Invited", "Player has been invited to the clan.")
+				else:
+					bot.reply("You're already in the clan?!")
+					bot.vb.NewPost( thread['threadid'], "Invited", "Player has been invited to the clan. (By someone else)")
 				bot.vb.MoveThread( thread['threadid'], 38 )
 				return
 		bot.reply("Error: No application found for your username.")
