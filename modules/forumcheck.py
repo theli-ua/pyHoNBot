8c4eb769fe078732deee1d4693b3f4350ea4ca2c
Ninja101 <pbninja101@gmail.com>
2012-09-13 01:16:32 +0100

    nooooob

--- modules/forumcheck.py
+++ modules/forumcheck.py
@@ -5,26 +5,26 @@
 def applycheck(bot, input):
 	"""Check if you application has been successful"""
 	if not input.admin: return
-	if not 'vb' in bot:
-		bot.reply('Error: Unable to check application')
-		return
-	bot.reply('Please wait, checking...')
-	if not bot.vb.Login(bot.config.forumuser,bot.config.forumpassword):
-		bot.reply('Error: Unable to check application')
-		print("Forum credentials are invaid")
-		return
-	traineeApps = bot.vb.GetThreads(36, 30)
-	for threadinfo in traineeApps:
-		thread = threadinfo['thread']
-		if thread['preview'].lower().find(input.nick.lower()) > 0:
-			bot.reply("Welcome to Project Epoch, %s! Inviting now." % input.nick)
+	try:
+		bot.reply('Please wait, checking...')
+		if not bot.vb.Login(bot.config.forumuser,bot.config.forumpassword):
+			bot.reply('Error: Unable to check application at this time')
+			print("Forum credentials are invaid")
 			return
-	mentorApps = bot.vb.GetThreads(38, 30)
-	for threadinfo in mentorApps:
-		thread = threadinfo['thread']
-		if thread['preview'].lower().find(input.nick.lower()) > 0:
-			bot.reply("Welcome to Project Epoch, %s! Inviting now." % input.nick)
-			return
+		traineeApps = bot.vb.GetThreads(36, 30)
+		for threadinfo in traineeApps:
+			thread = threadinfo['thread']
+			if thread['preview'].lower().find(input.nick.lower()) > 0:
+				bot.reply("Welcome to Project Epoch, %s! Inviting now." % input.nick)
+				return
+		mentorApps = bot.vb.GetThreads(38, 30)
+		for threadinfo in mentorApps:
+			thread = threadinfo['thread']
+			if thread['preview'].lower().find(input.nick.lower()) > 0:
+				bot.reply("Welcome to Project Epoch, %s! Inviting now." % input.nick)
+				return
+	except:
+		bot.reply('Error: Unable to check application at this time')
 applycheck.commands = ['apply']
 
 if __name__ == '__main__': 
