17357d24f59420ac40d501f5692e3b3ba2e5acaf
Ninja101 <pbninja101@gmail.com>
2012-10-03 17:04:59 +0100

    Didn't check if they're in a clan at all.

--- modules/channel.py
+++ modules/channel.py
@@ -26,7 +26,7 @@ def silence_smurfs(bot,chanid,nick):
         return
     query = {'nickname' : nick,'f': 'show_stats','table': 'ranked'}
     stats_data = bot.masterserver_request(query,cookie=True)
-    if stats_data['name'].lower() in bot.config.clanwhitelist:
+    if 'name' in stats_data and stats_data['name'].lower() in bot.config.clanwhitelist:
         return
     if 'rnk_wins' not in stats_data:
         return
