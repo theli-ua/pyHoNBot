2e8af362bbd362036f002b8bf8658de62f23c164
Ninja101 <pbninja101@gmail.com>
2012-11-24 22:21:51 +0000

    Altered anti-spam check.

--- modules/antispam.py
+++ modules/antispam.py
@@ -12,23 +12,22 @@ def silence(bot, chanid, nick):
 def checkSpam(bot, origin, data):
 	chanid = origin[2]
 	nick = bot.id2nick[origin[1]].lower()
-	t = time()
+	now = time()
 	if nick in bot.spamcd:
+		for k,t in enumerate(bot.spamcd[nick]):
+			if (now - bot.config.spam_length) > t:
+				del(bot.spamcd[nick][k])
 		if len(bot.spamcd[nick]) >= 4:
-			shouldSilence = True
-			for times in bot.spamcd[nick]:
-				if times < (t - 10):
-					shouldSilence = False
-			if shouldSilence:
-				silence(bot, chanid, nick)
+			silence(bot, chanid, nick)
 			del(bot.spamcd[nick])
 		else:
-			bot.spamcd[nick].append(t)
+			bot.spamcd[nick].append(now)
 	else:
-		bot.spamcd[nick] = [t]
+		bot.spamcd[nick] = [now]
 checkSpam.event = [ID.HON_SC_CHANNEL_MSG]
 checkSpam.thread = True
 
 def setup(bot):
 	bot.spamcd = {}
-	bot.config.module_config('spam_silence', [10, 'Silence for x minutes when spam detected, 0 = disabled'])
+	bot.config.module_config('spam_silence', [5, 'Silence for x minutes when spam detected, 0 = disabled'])
+	bot.config.module_config('spam_length', [5, 'Seconds until messages are removed from the spam stack'])
