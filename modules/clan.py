04a768fde38aef4d498e58cf48c49b692ebc9917
Ninja101 <pbninja101@gmail.com>
2012-12-09 16:26:52 +0000

    Big derp

--- modules/clan.py
+++ modules/clan.py
@@ -177,8 +177,10 @@ announce.commands = ['announce']
 
 def dnd(bot, input):
     """Mentors or Officers can set themselves to not appear in .mentors/.officers command"""
-    if not input.nick in bot.config.mentors or not input.nick in bot.config.officers:
-        return
+    id = bot.nick2id[input.nick]
+    if not id in bot.clan_roster or (id in bot.clan_roster and not bot.clan_roster[id]['rank'] in ['Officer', 'Leader']):
+        if not input.nick in bot.config.mentors and not input.nick in bot.config.officers:
+            return
     for key, nick in enumerate(bot.dnd):
         if input.nick == nick:
             bot.reply("You are now available in mentors/officers command.")
