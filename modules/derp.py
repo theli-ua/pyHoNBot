c31a34a141c316510bad585d957262d9621afa13
Ninja101 <pbninja101@gmail.com>
2013-01-15 18:28:24 +0000

    Added .roll for admins to troll with.

--- modules/derp.py
+++ modules/derp.py
@@ -1,4 +1,5 @@
 from random import randint
+from hon.packets import ID
 depend = ['honstringtables']
 derps = []
 def setup(bot):
@@ -15,3 +16,20 @@ def derp(bot,*args):
     """Derp"""
     bot.say(bot.stringtables[derps[randint(0,len(derps) - 1)]])
 derp.commands = ['derp']
+
+def roll(bot, input):
+	if not input.admin:
+		return
+	if not input.group(2):
+		return
+	if input.origin[0] == ID.HON_SC_CHANNEL_MSG:
+		bot.write_packet(ID.HON_CS_CHANNEL_ROLL, input.group(2), input.origin[2])
+	else:
+		chan = input.group(3)
+        if chan is not None:
+            chan = bot.chan2id[chan.lower()]
+        elif input.origin[0] == ID.HON_SC_CHANNEL_MSG:
+            chan = input.origin[2]
+        if chan is not None:
+            bot.write_packet(ID.HON_CS_CHANNEL_ROLL, input.group(2), chan)
+roll.rule = (['roll'],'([^\ ]+)?(?:\ +(.+))?')
