54c738946e84c09dd0b0beca3254094731e76d36
Ninja101 <pbninja101@gmail.com>
2013-02-15 15:58:38 +0000

    Error fix

--- modules/channel.py
+++ modules/channel.py
@@ -73,7 +73,7 @@ def silence_smurfs(bot,chanid,nick):
         return
     query = {'nickname' : nick,'f': 'show_stats','table': 'ranked'}
     stats_data = bot.masterserver_request(query,cookie=True)
-    if 'rnk_games_played' not in stats_data:
+    if not stats_data or 'rnk_games_played' not in stats_data:
         return
     if (int(stats_data['rnk_games_played']) - int(stats_data['rnk_discos'])) <= bot.config.silence_smurfs:
         bot.write_packet(ID.HON_CS_CHANNEL_SILENCE_USER, chanid, nick, 0x7fffffff)
